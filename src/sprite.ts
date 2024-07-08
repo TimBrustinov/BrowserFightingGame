26ebd9e7060b382af8ccbe1f3dcbb91ce04bf085
Tim Brustinov <timbrustinov@gmail.com>
2024-07-08 14:07:58 -0700

    changed image to have an on load

--- src/sprite.ts
+++ src/sprite.ts
@@ -29,6 +29,7 @@ class Sprite
 
     draw()
     {
+        this.image.onload = () => {
         ctx.drawImage(
             this.image,
             this.framesCurrent * (this.image.width / this.maxFrames),
@@ -40,6 +41,7 @@ class Sprite
             (this.image.width / this.maxFrames) * this.scale, 
             this.image.height * this.scale
         );
+       }
     }
 
     animateFrames()
